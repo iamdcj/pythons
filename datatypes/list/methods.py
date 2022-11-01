02bf456ff505764955eb17b6e9c0cbd37e975e97
David Christian Jones <david@dcj.codes>
2022-11-01 15:34:06 -0400

    method beginnings

---
+++ datatypes/list/methods.py
@@ -0,0 +1,15 @@
+intList = [1, 2, 3, 4, 5]
+
+print(len(intList)) # 5
+
+
+## append item
+intList.append(6)
+
+print(intList) ## [1, 2, 3, 4, 5, 6]
+
+
+## insert item
+intList.insert(1, 1.5)
+
+print(intList) ## [1, 1.5, 2, 3, 4, 5, 6]
