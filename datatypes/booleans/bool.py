2536b0457245656f8106caf2e4195219e6d3d52f
David Christian Jones <david@dcj.codes>
2022-11-01 13:02:51 -0400

    more things about

---
+++ datatypes/booleans/bool.py
@@ -0,0 +1,15 @@
+name = "David"
+
+print(name == "Josh")
+print(name == "David")
+
+
+if name == "Steffie":
+    print(type(True))
+
+
+if 1 == True:
+    print('truthy')
+
+if 0 == False:
+    print('falsy')
