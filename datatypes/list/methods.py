3ea9b11b625a0e94196e010231f73b9bc185b056
David Christian Jones <david@dcj.codes>
2022-11-02 09:27:24 -0400

    more methods

--- datatypes/list/methods.py
+++ datatypes/list/methods.py
@@ -3,39 +3,69 @@ from cmath import log
 
 intList = [1, 2, 3, 4, 5]
 
+# returns length of list
 print(len(intList))  # 5
 
 
-# append item
+# append item - returns None, modifies in place
 intList.append(6)
 
 print(intList)  # [1, 2, 3, 4, 5, 6]
 
 
-# insert item
+# insert item - returns None, modifies in place
 intList.insert(1, 1.5)
 
 print(intList)  # [1, 1.5, 2, 3, 4, 5, 6]
 
 
-# remove from end
+# remove from end - returns None, modifies in place
 intList.pop()
 
 print(intList)  # [1, 1.5, 2, 3, 4, 5]
 
-# remove from index
-popped = intList.pop(1)  # returns removed value
+# remove from index - returns removed value
+popped = intList.pop(1)  
 print(popped)  # 1.5
 
 print(intList)  # [1, 2, 3, 4, 5]
 
 
-# remove by value
+# remove by value - returns None, modifies in place
 intList.remove(1)
 
 print(intList)  # [2, 3, 4, 5]
 
-# clear list
+# clear list - clears entire list
 intList.clear()
 
 print(intList)  # []
+
+
+
+stringList = ['David', 'Steffie', 'Steffie', 'Nolé']
+
+# index - returns index of the item, if item is found in list
+print(stringList.index('Steffie'))  # 1
+
+
+# count - returns an integer reflecting how many times an item is within a list
+print(stringList.count('Steffie')) # 2
+
+
+# sort - sorts list (default: alphabetically) - returns None, modifies in place
+stringList.sort()
+print(stringList) # ['David', 'Nolé', 'Steffie', 'Steffie']
+
+
+# copy - copies list - returns new array
+copiedList = stringList.copy()
+print('COPY: ', copiedList)
+print('ORIGINAL: ', stringList)
+
+# reverse - reverses list - returns None, modifies in place
+reversed = stringList.reverse()
+print('REVERSE: ', stringList)
+
+
+
