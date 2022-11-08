1165504d05d798f56c66e4089102d19a2dfd1fd2
David Christian Jones <david@dcj.codes>
2022-11-08 14:36:16 -0500

    TIDY OPERATORS

---
+++ operators/comparison.py
@@ -0,0 +1,17 @@
+if 2 > 1:
+    print("greater than")
+
+if 2 < 4:
+    print("less than")
+
+if 1 == True:
+    print("equal to")
+
+if 1 >= 0:
+    print("greater than or equal to")
+
+if 1 <= 2:
+    print("less than or equal to")
+
+if 1 != 2:
+    print("does not equal")
--- operators/logical.py
+++ operators/logical.py
@@ -1,9 +1,28 @@
+# AND
+a = True
+b = True
+
+if a and b:
+    print(f"both are true")
+
+## both are true
+
+a = 0
+b = 1
+
+if a and b:
+    print(f"both are true")
+else: 
+    print(f"a is false so we stop there")
+
+
 a = True
 b = True
 
 if a or b:
     print(f"a is true so we stop")
 
+## OR
 a = 0
 b = 1
 
