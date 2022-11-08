588b98bb6cd05347f43d8d29f3bd63bafd0fde80
David Christian Jones <david@dcj.codes>
2022-11-08 14:44:27 -0500

    silly exercise

---
+++ control-flow/short-circuiting/if...else.py
@@ -0,0 +1,11 @@
+isMagician = 1
+isExpert = 0
+
+if isMagician and isExpert:
+  print("you master and magician")
+
+elif isMagician and not (isExpert):
+  print("keep going")
+
+elif not(isMagician):
+  print("get working")
