b219d4fee6f3c914649abe581b8b6635d8d90d50
David Christian Jones <david@dcj.codes>
2022-11-16 09:53:30 -0500

    more while

---
+++ statements/while/WHILE.md
@@ -0,0 +1,17 @@
+# `while`
+
+The `while` statement allows engineers to iterate over something until a certain condition is true
+
+```
+i = 0;
+
+ while i < 5:
+     print(i)
+     i += 1
+
+# 0
+# 1
+# 2
+# 3
+# 4
+```
--- statements/while/while.py
+++ statements/while/while.py
@@ -1,5 +1,23 @@
 i = 0
 
-while i < 50:
-    print(i)
+while i < 5:
     i += 1
+    if i == 3:
+        break
+    else:
+        print(i)
+
+
+x = 0
+
+while x < 5:
+    print("before continue", x)
+    x += 1
+    continue
+    print(x)
+
+# before continue 0
+# before continue 1
+# before continue 2
+# before continue 3
+# before continue 4
